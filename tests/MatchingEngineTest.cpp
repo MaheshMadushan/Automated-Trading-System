9aab87ca82ada2afa0894404013bbfa2d324318d
MaheshMadushan <madu140@gmail.com>
2025-01-21 19:38:57 +0530

    Added Buy Sell Matching Test case

--- tests/MatchingEngineTest.cpp
+++ tests/MatchingEngineTest.cpp
@@ -1,30 +1,22 @@
 #include "../src/matching/MatchingEngine.hpp"
 #include "Setup.hpp"
 
-TEST_F(BaseTest, Matching_Algorithm_Test)
+TEST_F(BaseTest, Matching_Algorithm_Test_Add_Mtaching_Buy_and_Sell_Order)
 {
-  long instrumentIndex(1);
+  long instrumentIndex(symbol_to_index_map["AAPL"]);
   MatchingEngine me{{instrumentIndex}};
-  for (auto sell_order : BaseTest::sell_orders)
-  {
-    me.matchOrder(sell_order);
-  }
-  for (auto buy_order : BaseTest::buy_orders)
-  {
-    me.matchOrder(buy_order);
-  }   
+                                // time_stamp,quantity,price,instrument,order_type
+  auto order1 = orderbook::Order{400, 4, 8, 1, orderbook::Order::OrderType::SELL};
+  auto order2 = orderbook::Order{400, 10, 10, 1, orderbook::Order::OrderType::BUY};
+  me.matchOrder(order1);
+  me.matchOrder(order2);
 
   auto sell_ordrs = me.getOrderBook(instrumentIndex)->getSellOrders();
   auto buy_ordrs = me.getOrderBook(instrumentIndex)->getBuyOrders();
-  while (sell_ordrs.empty() != true)
-  {
-    orderbook::Order sell_ordr = sell_ordrs.top();
-    sell_ordrs.pop();
-  }
 
-  while (buy_ordrs.empty() != true)
-  {
-    orderbook::Order buy_ordr = buy_ordrs.top();
-    buy_ordrs.pop();
-  }
+  auto resulting_buy_ordr = orderbook::Order{400, 6, 10, 1, orderbook::Order::OrderType::BUY};
+  EXPECT_TRUE(sell_ordrs.empty());
+  EXPECT_TRUE(buy_ordrs.size() == 1);
+  auto buy_ordr = buy_ordrs.top();
+  EXPECT_EQ(buy_ordr, resulting_buy_ordr);
 }
