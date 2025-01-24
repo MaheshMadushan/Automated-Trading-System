cc14a96d7c8392892b29880639fc573ce3fa1b0c
MaheshMadushan <madu140@gmail.com>
2025-01-24 22:30:49 +0530

    add price/time mtaching algorithm perf benhcmarking

--- src/benchmark/benchmark.cpp
+++ src/benchmark/benchmark.cpp
@@ -1,19 +1,22 @@
 #include <benchmark/benchmark.h>
-#include "../tests/Setup.hpp"
+#include "../matching/MatchingEngine.hpp"
 
-static void BM_StringCreation(benchmark::State& state) {
+static void BM_MatchingAlgorithmPriceTime(benchmark::State& state) {
+  std::map<std::string, int> symbol_to_index_map{{"AAPL" , 1}};
+  long instrumentIndex(1);
+  MatchingEngine me{{instrumentIndex}};
   for (auto _ : state)
-    std::string empty_string;
+  {
+    state.PauseTiming();
+    auto order = orderbook::Order{state.range(0), state.range(1), 
+          (int)state.range(2), state.range(3), static_cast<orderbook::Order::OrderType>(state.range(4))};
+    state.ResumeTiming();
+    me.matchOrder(order);
+  }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_MatchingAlgorithmPriceTime)
+                ->Args({400, 17, 10, 1, static_cast<int>(orderbook::Order::OrderType::BUY)})
+                ->Args({400, 17, 10, 1, static_cast<int>(orderbook::Order::OrderType::SELL)});
 
 BENCHMARK_MAIN();
